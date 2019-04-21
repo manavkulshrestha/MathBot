6885108ab69a46e7ee4fb14dbdf8810b96fc96f7
Manav Kulshrestha <manavkoolz@gmail.com>
2019-04-21 19:58:12 +0000

    Done.

--- client.c
+++ client.c
@@ -4,145 +4,170 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <netdb.h>
 
-#define MAX_QUESTIONS 2000
 #define MAX_QUESTION_LEN 50
-#define BYE_LEN 3
+#define FLAG_LEN 64
 
-int open_client_fd(char *host, int port) {
-	int fd;
+#define STATUS_START 6 // strlen("cs230 ");
+#define STATUS_LEN 6 // strlen("STATUS");
 
-	struct sockaddr_in server_addr;
-	struct addrinfo hints, *addr;
+/*
+    Arguments:
+        char *ip - host ip to connect to.
+        int port - port to connect to.
 
-	memset(&hints, 0, sizeof(hints));
+    Description:
+        Opens a socket and connects to the server using the host ip and port specified.
+        Note that the function exits if it can't create a socket, get host, or fails to
+        connect.
 
-	hints.ai_family = AF_INET;
-  	hints.ai_socktype = SOCK_STREAM;
-  	hints.ai_protocol = 0;
-  
- 	if((errno = getaddrinfo(host, NULL, &hints, &addr)) != 0) {
- 		fprintf(stderr, "Failed to get host information. %s.\n", gai_strerror(errno));
-		exit(errno);
- 	}
+    Returns:
+        int fd - the file descriptor for the server.
+*/
+int open_client_fd(char *ip, int port) {
+    int fd;
+    struct hostent *hp;
+    struct sockaddr_in serv_addr;
 
-  	if((fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol)) == -1) {
-		fprintf(stderr, "Failed to create socket. errno = %i.\n", errno);
-		exit(errno);
-	}
-
-	memset(&server_addr, 0, sizeof(server_addr));
-
-  	server_addr.sin_family = addr->ai_family;
-    server_addr.sin_addr.s_addr = &(((struct sockaddr_in *)addr->ai_addr)->sin_addr); //what
-    server_addr.sin_port = htons(port);
-
-    if(connect(clientfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1) {
-    	fprintf(stderr, "Failed to connect. errno = %i.\n", errno);
-    	exit(errno);
-    }	
-
-	freeaddrinfo(addr);
-	addr = NULL;
-
-	return fd;
+    // Create socket
+    if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        fprintf(stderr, "Error: couldn't create socket. errno = %i.\n", errno);
+        exit(errno);
     }
+
+    // Get server information
+    if((hp = gethostbyname(ip)) == NULL) {
+        fprintf(stderr, "Error: couldn't get hostname.\n");
+        exit(-2);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+
+    memcpy((char *)&serv_addr.sin_addr.s_addr, (char *)hp->h_addr_list[0], hp->h_length);
+
+    serv_addr.sin_port = htons(port);
+
+    // Connect to the server
+    if(connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+       fprintf(stderr, "ERROR: failed to connect. errno = %i.\n", errno);
+       exit(errno);
+    }
+
+    return fd;
 }
 
+/*
+    Arguments:
+        char *question -  character array containing the question (of specified form).
+
+    Description:
+        Solves the problem specified by the question.
+
+    Returns:
+        int answer - the solution to the problem question.
+        -1 - couldn't parse question correctly.
+
+*/
 int solve(char *question) {
-	int num1, num2, answer;
-	char operator;
+    int num1, num2, answer;
+    char operator;
 
-	sscanf(question, "cs230 STATUS %i %c %i\n", &num1, &operator, &num2);
+    sscanf(question, "cs230 STATUS %i %c %i\n", &num1, &operator, &num2);
 
-	switch(operator) {
-		case '+':
-			answer = num1+num2;
-			break;
-		case '-':
-			answer = num1-num2;
-			break;
-		case '*':
-			answer = num1*num2;
-			break;
-		case '/':
-			answer = num1/num2;
-			break;
-		case '%':
-			answer = num1%num2;
-			break;
-		default:
-			answer = -1;
-	} 
+    switch(operator) {
+        case '+':
+            answer = num1+num2;
+            break;
+        case '-':
+            answer = num1-num2;
+            break;
+        case '*':
+            answer = num1*num2;
+            break;
+        case '/':
+            answer = num1/num2;
+            break;
+        default:
+            answer = -1;
+    }
 
-	return answer;
+    return answer;
 }
 
 int main(int argc, char **argv) {
-	if(argc != 4) {
-		fprintf(stderr, "Usage: %s <host> <port>.\n", argv[0]);
-		exit(argc);
-	}
+    int fd, len, temp;
+    char buf[MAX_QUESTION_LEN], flag[FLAG_LEN];
 
-	char *host = argv[0];
-	int port = atoi(argv[1]);
-	char *ip = argv[2];
+    // Checking for appropriate number of arguments
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <id> <port> <host>\n", argv[0]);
+        exit(argc);
+    }
 
-	int len, temp;
-	char *buf = (char *) malloc(MAX_QUESTION_LEN*sizeof(char));
-	sprintf(buf, "cs230 HELLO %s\n", host);
+    // Getting file descriptor
+    fd = open_client_fd(argv[3], atoi(argv[2]));
 
-	int client_fd = open_client_fd(host, port);
+    // Initial identification message
+    sprintf(buf, "cs230 HELLO %s\n", argv[0]);
 
-	while(1) {
-		len = strlen(buf)+1;
-		switch(temp = send(client_fd, buf, len, 0)) {
-			case -1:
-				fprintf(stderr, "Error sending string. errno = %i.\n", errno);
-				exit(errno);
-			default:
-				if(temp != len) {
-					char *partial;
-					strncpy(partial, buf, temp);
-					fprintf(stderr, "Partial string sent: '%s'.\n", partial);
-					exit(temp);
-				}
-		}
+    while(1) {
+        // Send buffer
+        len = strlen(buf);
+        switch(temp = send(fd, buf, len, 0)) {
+            case -1:
+                fprintf(stderr, "Error: couldn't send string. errno = %i.\n", errno);
+                exit(errno);
+            default:
+                if(temp != len) {
+                    char *partial;
+                    strncpy(partial, buf, temp);
+                    fprintf(stderr, "Error: partial string sent: '%s'.\n", partial);
+                    exit(temp);
+                }
+        }
 
-		switch(recv(client_fd, buf, MAX_QUESTION_LEN, 0)) {
-			case -1:
-				fprintf(stderr, "Error recieving from server. errno = %i.\n", errno);
-				exit(errno);
-				break;
-			case 0:
-				fprintf(stderr, "Server terminated connection. Solution may be incorrect.\n");
-				exit(0);
-		}
-		len = strlen(buf);
-		buf[len] = '\0';
+        memset(buf, 0, sizeof(buf));
 
-		if(strcmp(buf+len-BYE_LEN, "BYE") == 0)//can be done betetr with sscanf maybe
-			break;
+        // Recieve to buffer
+        switch(recv(fd, buf, MAX_QUESTION_LEN, 0)) {
+            case -1:
+                fprintf(stderr, "Error: couldn't recieve from server. errno = %i.\n", errno);
+                exit(errno);
+                break;
+            case 0:
+                fprintf(stderr, "Error: server terminated connection. Solution may be incorrect.\n");
+                exit(0);
+        }
+        len = strlen(buf);
+        buf[len] = '\0';
 
-		if((temp = solve(buf)) == -1) {
-			fprintf(stderr, "Operator not recognized.\n");
-			exit(-1);
-		}
+        // Break out of loop if it doesn't contain a problem (thus, must contain the flag)
+        if(strncmp(buf+STATUS_START, "STATUS", STATUS_LEN) != 0)
+            break;
+        // if(!strstr(buf, "STATUS"))
+        //     break;
 
-		sprintf(buf, "cs230 %i\n", temp);
-	}
+        // Solve the problem
+        if((temp = solve(buf)) == -1) {
+            fprintf(stderr, "Error: couldn't parse question.\n");
+            exit(-1);
+        }
 
-	close(client_fd);
+        // Set buffer to the solution to the question
+        sprintf(buf, "cs230 %i\n", temp);
+    }
 
-	printf("Flag captured: '%s'.\n", buf);//do
+    // Close file descriptor
+    close(fd);
 
-	free(buf);
-	buf = NULL;
+    sscanf(buf, "cs230 %s", flag);
+    printf("Flag captured: %s\n", flag);
+    // df65ed32e8c67d962bc96127e1f860a747479da0a8efdf
 
-	return 0;
+    exit(0);
 }
